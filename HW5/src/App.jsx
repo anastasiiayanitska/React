077d2806dba620c46a4f7f151969a5fc29946514
Anastasiia Yanitska <martynovaanastasiia21@gmail.com>
2024-10-13 22:12:46 +0200

    HW5

---
+++ HW5/src/App.jsx
@@ -0,0 +1,16 @@
+import './styles/App.css'
+import Spotify from './components/spotify'
+import MenuBar from './components/menuBar'
+
+function App() {
+ return(
+  <div>
+    <Spotify />
+    
+    <MenuBar />
+    </div>
+
+ )
+}
+
+export default App
---
+++ HW5/src/components/spotify.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/spotify.svg"
+
+export default function Spotify() {
+   
+    return (
+        <div>
+            <img src={logo} alt="spotify" style={{margin:"55px 35px"}}/>
+        </div>
+    )
+}
